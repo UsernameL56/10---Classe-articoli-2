cb52f2d5eb4e72129b9dad83488b116f2fd83463
Corsi.20188 <Corsi.20188@E2-9PCWS2>
2023-11-21 12:55:09 +0100

    classe Articolo e ArticoloAlimentare

---
+++ Classi/ArticoloAlimentare.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classi
+{
+    public class ArticoloAlimentare : Articolo
+    {
+        // attributi
+        protected int _annoScadenza;
+
+        //metodi accessor
+        static int AnnoScadenza
+        {
+            get { return AnnoScadenza; }
+            set { AnnoScadenza = value; }
+        }
+
+        //costruttore
+        public ArticoloAlimentare(int _codice, string _descrizione, double _prezzoUnitario, int _annoScadenza) : base(_codice, _descrizione, _prezzoUnitario)
+        {
+            AnnoScadenza = _annoScadenza;
+        }
+
+        //metodi
+        public override void Sconta(bool cartaFedelta)
+        {
+            double sconto;
+            base.Sconta(cartaFedelta);
+
+            if (AnnoScadenza == DateTime.Now.Year)
+            {
+                sconto = (PrezzoUnitario * 20) / 100;
+                PrezzoUnitario -= sconto;
+            }
+
+        }
+    }
+}
