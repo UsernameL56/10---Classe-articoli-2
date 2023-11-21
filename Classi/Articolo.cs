7b1224d133cb5b029ca9bdc6417079d83d2e6bf4
Corsi.20188 <Corsi.20188@E2-9PCWS2>
2023-11-21 13:07:32 +0100

    classi

--- Classi/Articolo.cs
+++ Classi/Articolo.cs
@@ -6,7 +6,47 @@ using System.Threading.Tasks;
 
 namespace Classi
 {
-    public class Class1
+    public class Articolo
     {
+        //attributi
+        protected int _codice;
+        protected string _descrizione;
+        protected double _prezzoUnitario;
+
+        //metodi accessor
+        public int Codice
+        {
+            get { return Codice; }
+            set { Codice = value; }
+        }
+        public string Descrizione
+        {
+            get { return Descrizione; }
+            set { Descrizione = value; }
+        }
+        public double PrezzoUnitario
+        {
+            get { return PrezzoUnitario; }
+            set { PrezzoUnitario = value; }
+        }
+
+        //costruttore
+        public Articolo(int _codice, string _descrizione, double _prezzoUnitario)
+        {
+            Codice = _codice;
+            Descrizione = _descrizione;
+            PrezzoUnitario = _prezzoUnitario;
+        }
+
+        //metodi
+        public virtual void Sconta(bool cartaFedelta)
+        {
+            double sconto;
+            if (cartaFedelta)
+            {
+                sconto = (PrezzoUnitario * 5) / 100;
+                PrezzoUnitario -= sconto;
+            }
+        }
     }
 }
