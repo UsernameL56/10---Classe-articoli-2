eadbb19568796c5a20a39606e42cfdd00f772579
Corsi.20188 <Corsi.20188@E2-9PCWS2>
2023-11-21 13:41:06 +0100

    inizio classe ArticoloFresco

---
+++ Classi/ArticoloFresco.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classi
+{
+    public class ArticoloFresco : ArticoloAlimentare
+    {
+        // attributi
+        private int _giornoConsumo;
+
+        // metodi accessor
+        public int GiornoConsumo
+        {
+            get { return _giornoConsumo; }
+            set { _giornoConsumo = value; }
+        }
+
+        // costruttore
+        public ArticoloFresco(int _codice, string _descrizione, double _prezzoUnitario, int _annoScadenza, int _giornoConsumo) : base(_codice, _descrizione, _prezzoUnitario, _annoScadenza)
+        {
+            GiornoConsumo = _giornoConsumo;
+        }
+
+        // metodi
+        public override void Sconta(bool cartaFedelta)
+        {
+
+        }
+    }
+}
