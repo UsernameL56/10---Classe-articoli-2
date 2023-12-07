d495d4a170efa17200daa9f65c16f0e116b1848f
Corsi.20188 <Corsi.20188@E2-9PCWS2>
2023-12-07 10:48:26 +0100

    aggiunta classe scontrino con metodo aggiunta

---
+++ Classi/Scontrino.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classi
+{
+    public class Scontrino
+    {
+        // attributi
+        public Articolo[] _articolo;
+        public int indice;
+
+        // costruttore
+        public Scontrino()
+        {
+            _articolo = new Articolo[100];
+        }
+
+        public void Aggiunta(Articolo parametro)
+        {
+            _articolo[indice] = parametro;
+            indice++;
+        }
+    }
+}
